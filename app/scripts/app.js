8bf8f095a37aa9e64d436e0c475da6e2e512c268
Milos Bejda <mbejda@live.com>
2016-02-19 14:01:54 -0500

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projectApp
+ * @description
+ * # projectApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projectApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngDroplet',
+    'ui.bootstrap',
+    'ui.codemirror'
+  ])
+  .config(function($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
